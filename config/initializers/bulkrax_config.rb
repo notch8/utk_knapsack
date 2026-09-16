9dd1cb2c0685d12893316ccb994b90524f4833e2
Shana Moore <shana.lavina.moore@gmail.com>
2026-09-16 09:38:44 -0700

    🔧 Match production Bulkrax split, source ID, and parser settings
    Mirror utk-hyku production config:
    - Change split delimiter from string '\|' to regex /\s*[|]\s*/ to
      trim whitespace around pipes
    - Override default_field_mapping so unmapped fields also pipe-split
    - Set fill_in_blank_source_identifiers to generate tenant-scoped IDs
    - Add resource_types to qa_controlled_properties
    - Remove OAI and XML parsers (CSV and Bagit only)
    
    Assisted by: Opus 4.6

---
+++ config/initializers/bulkrax_config.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+Rails.application.config.to_prepare do
+  next unless Hyku.bulkrax_enabled?
+
+  Bulkrax.setup do |config|
+    config.parsers -= [
+      { name: "OAI - Dublin Core", class_name: "Bulkrax::OaiDcParser", partial: "oai_fields" },
+      { name: "OAI - Qualified Dublin Core", class_name: "Bulkrax::OaiQualifiedDcParser", partial: "oai_fields" },
+      { name: "XML", class_name: "Bulkrax::XmlParser", partial: "xml_fields" }
+    ]
+
+    config.fill_in_blank_source_identifiers = ->(obj, index) {
+      "#{Site.instance.account.name}-#{obj.importerexporter.id}-#{index}"
+    }
+
+    config.default_field_mapping = lambda do |field|
+      return if field.blank?
+      {
+        field.to_s =>
+        {
+          from: [field.to_s],
+          split: /\s*[|]\s*/,
+          parsed: Bulkrax::ApplicationMatcher.method_defined?("parse_#{field}"),
+          if: nil,
+          excluded: false
+        }
+      }
+    end
+
+    config.qa_controlled_properties += ['resource_types']
+  end
+end
